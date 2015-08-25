b24f7f9379197e01c9615649df0661fe4de86602
halfak <aaron.halfaker@gmail.com>
2015-08-25 18:38:57 -0500

    Adds LICENSE reading to setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
         author="Yuvi Panda",
         author_email="yuvipanda@gmail.com",
         url="http://github.com/yuvipanda/python-mwapi",
-        packages=["mwapi", ],
-        license="MIT License",
+        packages=["mwapi"],
+        license=open("LICENSE").read(),
         description = "Simple wrapper for the Mediawiki API",
         long_description = open("README").read(),
         install_requires = ["requests"]
