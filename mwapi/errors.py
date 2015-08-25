8ba7b44712d78071a11ed93828df25d10a9ea9ff
halfak <aaron.halfaker@gmail.com>
2015-08-25 18:36:53 -0500

    Adds errors module.

---
+++ mwapi/errors.py
@@ -0,0 +1,21 @@
+"""
+Errors
+======
+There's two types of errors.
+
+.. autoclass:: APIError
+
+.. autoclass:: LoginError
+"""
+
+class APIError(RuntimeError):
+    """
+    Thrown when the MediaWiki API returns an error.
+    """
+    pass
+
+class LoginError(APIError):
+    """
+    Thrown when an error occurs during login.
+    """
+    pass
