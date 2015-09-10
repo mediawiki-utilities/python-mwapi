2ea15ac4781d8942b6232e954574342dcf88704a
halfak <aaron.halfaker@gmail.com>
2015-09-10 16:59:12 -0500

    Fixes print statement for demo_timeout.  Timeout is 0.5 seconds.

--- demo_timeout.py
+++ demo_timeout.py
@@ -7,7 +7,7 @@ my_agent = 'mwapi demo script <ahalfaker@wikimedia.org>'
 session = mwapi.Session('https://10.11.12.13',  user_agent=my_agent,
                         timeout=0.5)
 
-print("Making a request that should hang for 2 seconds and then timeout.")
+print("Making a request that should hang for 0.5 seconds and then timeout.")
 try:
     session.get(action="fake")
 except mwapi.errors.TimeoutError as e:
